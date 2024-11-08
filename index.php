f149816b2a52106da9eb6a9aa929f4a34bad2057
franng1028 <francisngenhan@gmail.com>
2024-11-08 09:19:30 +0800

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!-- index.php -->
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <title>在线计算器</title>
+    <link rel="stylesheet" type="text/css" href="GA.css">
+</head>
+<body>
+
+    <div class="banner">
+        <h1>欢迎使用多功能计算器</h1>
+        <p>选择你的计算器类型，体验简单快捷的转换操作</p>
+        <a href="number_calculator.php">数字计算器 | </a>
+        <a href="length_calculator.php?type">长度转换 | </a>
+        <a href="weight_calculator.php?type">重量转换 | </a>
+        <a href="temperature_calculator.php">温度转换</a>
+    </div>
+
+</body>
+</html>
