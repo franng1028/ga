5b9aa117a99af9637e337fbce9e4ce9365fdffa9
rnmtq-ddqwmm <152743714+rnmtq-ddqwmm@users.noreply.github.com>
2024-11-25 13:21:53 +0800

    Update number_calculator.php

--- number_calculator.php
+++ number_calculator.php
@@ -1,13 +1,11 @@
-<!-- basic_calculators.php -->
 <!DOCTYPE html>
-<html lang="zh">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Digital Calculator</title>
     <link rel="stylesheet" href="GA.css">
 </head>
 <body>
-    <!-- 导航栏 -->
     <div class="navbar">
         <div class="left">
         <a href="index.php" class="plain-link">Welcome to The Multi-Function Caculator</a>
@@ -25,7 +23,6 @@
         <ul>
             <li><a href="basic_calculator.php">Basic Calculator</a></li>
             <li><a href="scientific_calculator.php">Scientific Calculator</a></li>
-            <!-- 添加更多的基础计算器 -->
         </ul>
     </div>
 </body>
